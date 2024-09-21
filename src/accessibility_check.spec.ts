2512a49f0a307372b7546f47ce528ce31493845e
pdatta <pdatta@asurint.com>
2024-09-21 11:39:37 -0700

    changed the default to calfus

--- src/accessibility_check.spec.ts
+++ src/accessibility_check.spec.ts
@@ -46,7 +46,7 @@ describe('WebAuditorService', () => {
   };
 
   it('should generate Lighthouse reports and ensure all accessibility scores are >= 75', async () => {
-    const url = process.env.TEST_URL || 'https://github.com/';
+    const url = process.env.TEST_URL || 'https://calfus.com/';
     const maxDepth = process.env.MAX_DEPTH || 0;
     const parentUuid = uuidv4();
     const response = service.makeScann(url, Number(maxDepth), parentUuid, false);
